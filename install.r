fc234d9dbd2c8bbfeb7944c6435eb934b05f2b1d
Gibran Hemani <g.hemani@bristol.ac.uk>
2026-01-07 11:54:00 +0000

    upd

---
+++ install.r
@@ -0,0 +1,17 @@
+# Set CRAN repository to Posit Package Manager with pre-built binaries from a specific date
+options(repos = c(CRAN = sprintf("https://packagemanager.posit.co/cran/2025-12-07/bin/linux/noble-%s/%s", R.version["arch"], substr(getRversion(), 1, 3))))
+
+# Install required packages from CRAN to user directory
+install.packages(c(
+    "remotes",
+    "data.table",
+    "R.utils",
+    "ggplot2",
+    "ggrepel",
+    "readxl",
+    "here",
+    "rmarkdown"
+))
+
+# Install specific version of meffonym package from GitHub
+remotes::install_github("perishky/meffonym@9faface")
