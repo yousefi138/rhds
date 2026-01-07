d66bdd420351f3137c702a8879b7591305cfc558
Gibran Hemani <g.hemani@bristol.ac.uk>
2026-01-07 11:54:41 +0000

    upd

--- run-all.sh
+++ run-all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 source config.env
-cd scripts
-bash download-data.sh
-Rscript download-pan-cancer-clinical.r $datadir $resultsdir
+bash scripts/download-data.sh $datadir $resultsdir
+Rscript scripts/download-pan-cancer-clinical.r $datadir $resultsdir
