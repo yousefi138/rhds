fb9b8fe3cbb02ba2b35b3c35344beb76c42148b3
Gibran Hemani <g.hemani@bristol.ac.uk>
2026-01-11 14:32:31 +0000

    upd

--- run-all.sh
+++ run-all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-mkdir -p docs
-
+# Get project variables
 source config.env
+
+# Create necessary directories if they don't exist
+mkdir -p $docsdir
+mkdir -p $resultsdir
+mkdir -p $datadir
+
+# Run all scripts
 cd scripts
 bash download-data.sh $datadir $resultsdir
 Rscript download-pan-cancer-clinical.r $datadir $resultsdir
